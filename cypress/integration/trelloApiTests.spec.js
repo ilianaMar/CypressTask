a3824cf84a354606cd1127a293a1642cbd8f6565
Iliana Markova <iliana.markova@estafet.com>
2022-08-01 17:49:49 +0300

    add simple api tests

--- cypress/integration/trelloApiTests.spec.js
+++ cypress/integration/trelloApiTests.spec.js
@@ -1,12 +1,13 @@
 const trelloAuthConfig = require('../support/config/trello-auth.json')
 
+
 describe('Trello api tests', () => {
     it('check user information', () => {
         let key  = trelloAuthConfig.key
         let token = trelloAuthConfig.token
         cy.request({
             method: 'GET',
-            url: 'https://api.trello.com/1/members/me',
+            url: `${Cypress.config('trelloApiUrl')}1/members/me`,
             qs: {
                 key: `${key}`,
                 token: `${token}`,
